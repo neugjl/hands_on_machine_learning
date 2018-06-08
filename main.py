3511726c72ee15f568a2daea1cd1b93fb107ca89
neugjl <neugjl@126.com>
2018-06-08 16:28:57 +0800

    Python for Data Analysis

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
-"""
-Write a program that prints the numbers from 1 to 20. But for multiples of three, print 'Fizz'
-instead of the number and for the multiples of five print 'Buzz'.
-For numbers which are multiples of both three and five print 'FizzBuzz'.
-"""
-from __future__ import print_function
-import torch
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net,self).__init__()
-        # 1 input image channel, 6 output channels, 5*5 square convolution kernel
-        self.conv1 = nn.Conv2d(1,6,5)
-        self.conv2 = nn.Conv2d(6,16,5)
-        # an affine operetion: y = Wx +b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120,84)
-        self.fc3 = nn.Linear(84,10)
+N, D_in, H, D_out = 64, 1000, 100,10
 
-    def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)),2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    def num_flat_features(self,x):
-        size = x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+x = np.random.randn(N, D_in)
+y = np.random.randn(N, D_out)
 
-net = Net()
-print(net)
+w1 = np.random.randn(D_in, H)
+w2 = np.random.randn(H,D_out)
 
-params = list(net.parameters())
-print(params)
+learning_rate = 1e-6
+for t in range(50):
+    h = x.dot(w1)
+    h_relu = np.maximum(h, 0)
+    y_pred = h_relu.dot(w2)
 
+    loss = np.square(y_pred-y).sum()
+    print(t,loss)
+
+    grad_y_pred = 2.0 * (y_pred-y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.copy()
+    grad_h[h < 0] = 0
+    grad_w1 = x.T.dot(grad_h)
+
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+
+
+
+template = '{0:0.2f} {1:s} are worth US${2:d}'
+result=template.format(4.5560,'Argentine Pesos',1)
+strings = ['a','as','bat','car','dove','python']
+lengths = [len(x) for x in strings]
+unique_lenghts = {len(x) for x in strings}
+unique_lenghts2 = set(lengths)
+
+a = []
+def func():
+    global a
+    a = None
+func()
+
+b= [1,2,2,3,3,4,5]
+c= b[2:4]
+c[1] = 10
+a_b = np.array([1,2,2,3,3,4,5])
+a_c = a_b[2:4]
+a_c[1] = 10
+
+import pandas as pd
+obj = pd.Series([4,7,-5,3],index=('a','b','c','d'))
