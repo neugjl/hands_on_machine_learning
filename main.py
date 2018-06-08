d23d3366986504efa7d8cd54538a14589e0f76a4
neugjl <neugjl@126.com>
2018-06-08 22:44:24 +0800

    scipy io practice

--- main.py
+++ main.py
@@ -61,3 +61,10 @@ S = np.sin(X)
 plt.plot(X,C)
 plt.plot(X,S)
 plt.show()
+
+from scipy import io as spio
+import numpy as np
+a = np.ones((3,3))
+spio.savemat('file.mat',{'a':a})
+data = spio.loadmat('file.mat')
+
